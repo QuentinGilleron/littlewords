b81b5000f406651a675bfb53b021eab806b0f585
QuentinGilleron <quentin.gilleron5627@gmail.com>
2023-02-08 17:20:08 +0100

    create app.dart et nouveaux fichiers de redirection future

---
+++ lib/app.dart
@@ -0,0 +1,48 @@
+// import
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:littlewords/provider/username.provider.dart';
+
+
+class LittleWordsApp extends StatelessWidget {
+  const LittleWordsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProviderScope(
+        child: MaterialApp(
+          title: 'Little Words',
+          home: _Routing(),
+        )
+    );
+  }
+}
+
+//
+class _Routing extends ConsumerWidget {
+  const _Routing({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(usernameProvider).when(data: _whenData, error: _whenError, loading: _whenLoading) ;
+  }
+
+  Widget _whenData(String? username) {
+
+    if(null == username){
+      return LoginRoute();
+    }
+    return HomeRoute();
+  }
+
+  Widget _whenError(Object error, StackTrace stackTrace) {
+    return ErrorRoute();
+
+  }
+
+  Widget _whenLoading() {
+    return LoadingRoute();
+  }
+}
