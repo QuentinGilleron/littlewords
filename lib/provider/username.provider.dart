db45cf49bb18ddea994ad24190673fa5a84b157c
QuentinGilleron <quentin.gilleron5627@gmail.com>
2023-02-08 16:55:20 +0100

    add dependencies

---
+++ lib/provider/username.provider.dart
@@ -0,0 +1,8 @@
+// Doit aller chercher le username dans les shared prefs
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final usernameProvider = FutureProvider<String>((ref) async{
+
+
+
+});
