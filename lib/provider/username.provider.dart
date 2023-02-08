bff6a7ffaefe81b003b42a89745fff4342d6ef83
QuentinGilleron <quentin.gilleron5627@gmail.com>
2023-02-08 17:00:41 +0100

    create new provider

--- lib/provider/username.provider.dart
+++ lib/provider/username.provider.dart
@@ -1,8 +1,10 @@
 // Doit aller chercher le username dans les shared prefs
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-final usernameProvider = FutureProvider<String>((ref) async{
-
-
-
+final usernameProvider = FutureProvider<String?>((ref) async{
+  var prefs = await SharedPreferences.getInstance();
+  const String key = 'username';
+  final String? username = prefs.getString(key);
+  return username;
 });
