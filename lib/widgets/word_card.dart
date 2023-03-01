3ed542dd6069b48bea53100a65b17063afb18e37
QuentinGilleron <quentin.gilleron5627@gmail.com>
2023-03-01 17:09:29 +0100

    n,

---
+++ lib/widgets/word_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class WordCard extends StatelessWidget {
+  const WordCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: B
+      ),
+    );
+  }
+}
