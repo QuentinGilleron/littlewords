a21f8bce7ec735967794d1119c1ace0860a25bbf
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2023-03-08 13:16:13 +0100

    Mise en place de la BDD

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:littlewords/app.dart';
+import 'package:littlewords/db/db.helper.dart';
 
 void main() {
+
+  // Initialize the database
+  WidgetsFlutterBinding.ensureInitialized();
+  DbHelper.initDb();
+
   runApp(const LittleWordsApp());
 }
